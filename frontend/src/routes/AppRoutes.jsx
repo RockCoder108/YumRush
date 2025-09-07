5b6a0370d94d71361a1886b0560e7efe2d2961a3
RockCoder108 <samyak80jain@gmail.com>
2025-09-08 00:31:09 +0530

    Add authentication pages and shared styles for user and food partner login/register

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import UserRegister from '../pages/auth/UserRegister';
+import UserLogin from '../pages/auth/UserLogin';
+import FoodPartnerRegister from '../pages/auth/FoodPartnerRegister';
+import FoodPartnerLogin from '../pages/auth/FoodPartnerLogin';
 function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/user/register" element={<h1>User Register</h1>} />
-        <Route path="/user/login" element={<h1>User Login</h1>} />
-        <Route path="/foodpartner/register" element={<h1>Food Partner Register</h1>} />
-        <Route path="/foodpartner/login" element={<h1>Food Partner Login</h1>} />
+        <Route path="/user/register" element={<UserRegister />} />
+        <Route path="/user/login" element={<UserLogin />} />
+        <Route path="/foodpartner/register" element={<FoodPartnerRegister />} />
+        <Route path="/foodpartner/login" element={<FoodPartnerLogin />} />
       </Routes>
     </Router>
   );
