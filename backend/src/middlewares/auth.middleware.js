d7379d4456a18c88c1870f06e7c7af9f53bc04c5
RockCoder108 <samyak80jain@gmail.com>
2025-09-08 12:22:45 +0530

    Add likeCount, savesCount

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -37,7 +37,11 @@ async function authUserMiddleware(req, res, next){
     try {
         const decode = jwt.verify(token, process.env.JWT_SECRET);
         const user = await userModel.findById(decode.id);
-
+        if (!user) {
+          return res.status(401).json({
+            message: "USER NOT FOUND, PLEASE LOGIN AGAIN",
+          });
+        }
         req.user = user;
         next();
     } catch (error) {
--- backend/src/models/food.model.js
+++ backend/src/models/food.model.js
@@ -1,24 +1,35 @@
 import mongoose from "mongoose";
 import foodPartner from "./foodpartner.model.js";
 
-const foodSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
+const foodSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    video:{
-        type: String,
-        required: true
+    video: {
+      type: String,
+      required: true,
     },
-    description:{
-        type: String,
-        required: true,
+    description: {
+      type: String,
+      required: true,
     },
-    foodPartner:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "foodPartner",
-    }
-},{timestamps:true});
+    foodPartner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "foodPartner",
+    },
+    likeCount: {
+      type: Number,
+      default: 0,
+    },
+    savesCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
 const FoodModel = mongoose.model("FoodModel", foodSchema);
 
